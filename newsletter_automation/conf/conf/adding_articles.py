e292b321673b72ade466d7c9210d34d879592653
aruulbhagkya <aruulbhagkya511@gmail.com>
2022-10-14 14:30:24 +0530

    newsletter generator application

---
+++ newsletter_automation/conf/conf/adding_articles.py
@@ -0,0 +1,28 @@
+email  = "aruul.bhagkya@qxf2.com"
+password = "AganSarwin@2021"
+article1={'url':'https://www.python.org/downloads/',
+'title': 'postman api tool',
+'description':'tool which is used to get,post,put and delete request',
+'runtime':'10',
+'category':'pastweek'}
+article2={'url':'https://www.techbeamers.com/handle-alert-popup-selenium-python/',
+'title':'handle-alert-popup-selenium',
+'description':'handle-alert-popup-selenium',
+'runtime':'15',
+'category':'currentweek'}
+article3={'url':'https://www.pythontutorial.net/advanced-python/python-mutable-and-immutable/',
+'title':'advanced-python/python-mutable-and-immutable',
+'description':'advanced-python/python-mutable-and-immutable',
+'runtime':'5',
+'category':'automation corner'}
+article4={'url':'https://www.browserstack.com/guide/alerts-and-popups-in-selenium',
+'title':'alerts-and-popups-in-selenium',
+'description':'alerts-and-popups-in-selenium',
+'runtime':'7',
+'category':'comic'}
+article5={'url':'https://www.pythontutorial.net/python-basics/python-numbers/',
+'title':'python-basics/python-numbers',
+'description':'python-basics/python-numbers',
+'runtime':'5',
+'category':'uncategorized'}
+article_list = [article1,article2,article3,article4,article5]
