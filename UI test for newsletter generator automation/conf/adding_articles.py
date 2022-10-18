b7a77ca1ca256acd00cf7aa5cfc24ac196252012
aruulbhagkya <40850620+aruulbhagkya@users.noreply.github.com>
2022-10-18 10:50:12 +0530

    Update adding_articles.py

--- UI test for newsletter generator automation/conf/adding_articles.py
+++ UI test for newsletter generator automation/conf/adding_articles.py
@@ -1,28 +1,9 @@
 email  = "Enter the username"
 password = "Enter the password"
-article1={'url':'https://www.python.org/downloads/',
-'title': 'postman api tool',
-'description':'tool which is used to get,post,put and delete request',
-'runtime':'10',
-'category':'pastweek'}
-article2={'url':'https://www.techbeamers.com/handle-alert-popup-selenium-python/',
-'title':'handle-alert-popup-selenium',
-'description':'handle-alert-popup-selenium',
-'runtime':'15',
-'category':'currentweek'}
-article3={'url':'https://www.pythontutorial.net/advanced-python/python-mutable-and-immutable/',
-'title':'advanced-python/python-mutable-and-immutable',
-'description':'advanced-python/python-mutable-and-immutable',
-'runtime':'5',
-'category':'automation corner'}
-article4={'url':'https://www.browserstack.com/guide/alerts-and-popups-in-selenium',
-'title':'alerts-and-popups-in-selenium',
-'description':'alerts-and-popups-in-selenium',
-'runtime':'7',
-'category':'comic'}
-article5={'url':'https://www.pythontutorial.net/python-basics/python-numbers/',
-'title':'python-basics/python-numbers',
-'description':'python-basics/python-numbers',
-'runtime':'5',
-'category':'uncategorized'}
+current_timestamp =str(int(time.time()))
+article1 = {'URL':'https://www.python.org/downloads/'+current_timestamp,'TITLE':'postman api tool','DESCRIPTION':'tool which is used to get,post,put and delete request','RUNTIME':'10','CATEGORY':'pastweek'}
+article2 = {'URL':'https://www.techbeamers.com/handle-alert-popup-selenium-python/'+current_timestamp,'TITLE':'handle-alert-popup-selenium','DESCRIPTION':'handle-alert-popup-selenium','RUNTIME':'15','CATEGORY':'currentweek'}
+article3 = {'URL':'https://www.pythontutorial.net/advanced-python/python-mutable-and-immutable/'+current_timestamp,'TITLE':'advanced-python/python-mutable-and-immutable','DESCRIPTION':'advanced-python/python-mutable-and-immutable','RUNTIME':'5','CATEGORY':'automation corner'}
+article4 = {'URL':'https://www.browserstack.com/guide/alerts-and-popups-in-selenium'+current_timestamp,'TITLE':'alerts-and-popups-in-selenium','DESCRIPTION':'alerts-and-popups-in-selenium','RUNTIME':'7','CATEGORY':'comic'}
+article5 = {'URL':'https://www.pythontutorial.net/python-basics/python-numbers/'+current_timestamp,'TITLE':'python-basics/python-numbers','DESCRIPTION':'python-basics/python-numbers','RUNTIME':'5','CATEGORY':'uncategorized'}
 article_list = [article1,article2,article3,article4,article5]
